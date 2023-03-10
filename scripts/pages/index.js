daf34d410ad72f17810c54cd33aeb896618656b6
Osiri70cl <remi.pince@outlook.com>
2023-03-10 13:04:31 +0100

    modif suite mentoirat

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,10 +1,2 @@
 import { API } from "../api/Api.js";
 import { createHeader } from "../components/header.js";
-
-async function init() {
-  // Create the main page header, then get photographers data and display them
-  createHeader("mainPage");
-  displayData(await API.getAllData("homePage"));
-}
-
-init();
